d90d9fdc61d92d4ee756ff697ce70fe1eb71bab7
Shea Bunge <shea@sheabunge.com>
2024-10-03 15:49:36 +1000

    Add styles and UI elements to imported snippets list.

--- src/js/components/ImportMenu/ImportMenu.tsx
+++ src/js/components/ImportMenu/ImportMenu.tsx
@@ -11,13 +11,37 @@ interface ImportedSnippetsListProps {
 }
 
 const ImportedSnippetsList: React.FC<ImportedSnippetsListProps> = ({ snippets }) =>
-	<div className="imported-snippets-list">
-		{snippets.map(snippet =>
-			<div className="imported-snippet" key={snippet.uuid}>
-				<p><strong>{snippet.name}</strong></p>
-			</div>
-		)}
-	</div>
+	snippets.length > 0 &&
+	<form className="imported-snippets-list">
+		<table className="widefat striped">
+			<thead>
+			<tr>
+				<td id="cb" className="check-column"><input type="checkbox" /></td>
+				<th className="snippet-column">{__('Snippet', 'code-snippets')}</th>
+				<th className="import-button-column"></th>
+				<th className="expand-icon-column"></th>
+			</tr>
+			</thead>
+			<tbody>
+						{snippets.map(snippet =>
+							<tr className="imported-snippet" key={snippet.uuid}>
+								<th scope="row" className="check-column"><input type="checkbox" /></th>
+								<th className="snippet-column">
+									<h3 role="button">{snippet.name}</h3>
+								</th>
+								<td className="import-button-column">
+									<button className="button button-primary">Import</button>
+								</td>
+								<td className="expand-icon-column"><span role="button"></span></td>
+							</tr>
+						)}
+			</tbody>
+		</table>
+
+		<p className="submit">
+			<button className="button button-primary button-large">{__('Import selected', 'code-snippets')}</button>
+		</p>
+	</form>
 
 const parseImportFile = (exported: unknown): ImportedSnippet[] =>
 	'object' === typeof exported && null !== exported && 'snippets' in exported && Array.isArray(exported.snippets) ?
